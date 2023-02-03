e06a0c8bff697d8cefcbb72b81765cfa57a18b53
louislallier <louis.lallier@gmail.com>
2023-02-03 17:39:20 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <div className="App">
+      <h1>To Do List</h1>
+      <div>
+        <input type="text" placeholder="new task" />
+        <button>Add Task</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
